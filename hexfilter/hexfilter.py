9b49be85c8e3a345a666b63ee0fbe0c1aa8e3f2c
Erik Stromdahl <erik.stromdahl@gmail.com>
2016-08-05 14:46:06 +0200

    hexfilter.py: dump_desc support
    Added support for filtering based on the hex dump description
    string.
    
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

--- hexfilter/hexfilter.py
+++ hexfilter/hexfilter.py
@@ -1,6 +1,21 @@
 
 import re
 import string
+# Check if we are running Python 2 or Python 3
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str, bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
 
 from abc import ABCMeta, abstractmethod
 
@@ -8,12 +23,12 @@ from abc import ABCMeta, abstractmethod
 # Linux definitions:
 
 # Linux kernel dump_hex regex'es.
-# Sample string:
+# Sample string with time stamp:
 # [    7.852404] sdio wr 00000000: 06 00 00 00 44 12 00 00                          ....D...
-# Timestamp regex:
-linux_ts_regex_pattern = '.*\[(\s*\d+\.\d+)\]\s+'
-# linux hex_dump log regex
-linux_hex_dump_regex_pattern = '.+([0-9a-f]{8}):\s(.+)'
+# Without timestamp:
+# sdio wr 00000000: 06 00 00 00 44 12 00 00                          ....D...
+linux_hex_dump_regex_pattern = '(.+)([0-9a-f]{8}):\s(.+)'
+linux_hex_dump_ts_regex_pattern = '.*\[(\s*\d+\.\d+)\]\s+(.+)([0-9a-f]{8}):\s(.+)'
 
 # Linux hex_dump uses lower case (a-f) for all hex values
 linux_valid_hex_data_chars = '0123456789abcdef '
@@ -34,7 +49,6 @@ class HexFilter:
     __metaclass__ = ABCMeta
 
     def __init__(self,
-                 ts_regex_pattern=None,
                  hex_dump_regex_pattern=default_hex_dump_regex_pattern,
                  valid_hex_data_chars=default_valid_hex_data_chars,
                  valid_ascii_chars=default_valid_ascii_chars,
@@ -48,7 +62,6 @@ class HexFilter:
         classes.
 
         Keyword arguments:
-        ts_regex_pattern        -- (string) Regex pattern for time stamps
         hex_dump_regex_pattern  -- (string) Regex pattern for hex dumps
         valid_hex_data_chars    -- (string) Valid hex digits + separator
         valid_ascii_chars       -- (string) Valid chars in the ascii part of the
@@ -69,11 +82,6 @@ class HexFilter:
         self.valid_ascii_chars = valid_ascii_chars
         self.max_num_hex_dump_values = max_num_hex_dump_values
 
-        if ts_regex_pattern:
-            self.ts_regex = re.compile(ts_regex_pattern)
-        else:
-            # A timestamp regex is not mandatory
-            self.ts_regex = None
         if hex_dump_regex_pattern:
             self.dump_regex = re.compile(hex_dump_regex_pattern)
         else:
@@ -86,7 +94,7 @@ class HexFilter:
         self.prev_ts = None
         self.data_available = False
 
-    def update_ts(self, line):
+    def update_ts(self, ts):
         """ Protected/private method used by inheriting classes.
 
         Reads the timestamp associated with the hex dump log and
@@ -96,14 +104,8 @@ class HexFilter:
         If timestamps_round_us > 0, the delta time will be rounded to the
         nearest timestamps_round_us microsecond.
         """
-        if not self.ts_regex:
-            return False
 
-        ts_match = self.ts_regex.match(line)
-        if ts_match is None:
-            return False
-
-        self.ts = ts_match.group(1)
+        self.ts = ts
         if self.prev_ts is None:
             self.ts_diff = 0.0
         else:
@@ -154,14 +156,57 @@ class HexFilterLinux(HexFilter):
     from a linux kernel log.
     """
     def __init__(self, skip_timestamps=False, abs_timestamps=False,
-                 timestamps_round_us=0):
+                 timestamps_round_us=0, log_has_timestamps=True,
+                 dump_desc=None):
+        """ HexFilterLinux constructor
+
+        Constructor for linux kernel log parser .
+
+        Keyword arguments:
+        skip_timestamps         -- (bool) Don't add timestamps to the output
+                                   (default False)
+        abs_timestamps          -- (bool) Add absolute timestamps to the output
+                                   instead of delta times. This argument will not
+                                   have any effect if skip_timestamps is True
+                                   (default False)
+        timestamps_round_us     -- (int) Timestamp rounding factor in microseconds.
+                                   All timestamps will be rounded to the nearest
+                                   timestamps_round_us microsecond
+                                   (default 0)
+        log_has_timestamps      -- (bool) The log files has printk timestamps, i.e,
+                                   the kernel was built with CONFIG_PRINTK_TIME
+                                   (default True)
+        dump_desc               -- (string or array) Description string(s) for each dump.
+                                   If set, only dumps containing the description(s)
+                                   will be filtered out.
+                                   If dump_desc is an array (of strings), each string
+                                   in the array will be checked against the description
+                                   string in the dump. If any string matches, the dump
+                                   will be considered valid.
+                                   (default None)
+        """
+        if log_has_timestamps:
+            regex_pattern = linux_hex_dump_ts_regex_pattern
+        else:
+            regex_pattern = linux_hex_dump_regex_pattern
+
         HexFilter.__init__(self,
-                           ts_regex_pattern=linux_ts_regex_pattern,
-                           hex_dump_regex_pattern=linux_hex_dump_regex_pattern,
+                           hex_dump_regex_pattern=regex_pattern,
                            valid_hex_data_chars=linux_valid_hex_data_chars,
                            skip_timestamps=skip_timestamps,
                            abs_timestamps=abs_timestamps,
                            timestamps_round_us=timestamps_round_us)
+
+        self.log_has_timestamps = log_has_timestamps
+        if dump_desc:
+            self.dump_desc_regexes = []
+            if isinstance(dump_desc, basestring):
+                self.dump_desc_regexes.append(re.compile(dump_desc))
+            elif isinstance(dump_desc, (list, tuple)):
+                for dump_desc_item in dump_desc:
+                    self.dump_desc_regexes.append(re.compile(dump_desc_item))
+        else:
+            self.dump_desc_regexes = None
 
     def parse_line(self, line):
         """ Parses a line of the log file and tries to interpret the hex data.
@@ -173,16 +218,33 @@ class HexFilterLinux(HexFilter):
         internally. In this case, True will be returned.
         """
 
-        if not self.skip_timestamps and self.ts_regex:
-            if not self.update_ts(line):
-                return False
-
         dump_match = self.dump_regex.match(line)
         if dump_match is None:
             return False
 
-        self.dump_addr = dump_match.group(1)
-        dump_data = dump_match.group(2)
+        match_idx = 1
+        if self.log_has_timestamps:
+            log_ts = dump_match.group(match_idx)
+            match_idx += 1
+            if not self.skip_timestamps:
+                if not self.update_ts(log_ts):
+                    return False
+
+        dump_desc_str = dump_match.group(match_idx)
+        match_idx += 1
+        if self.dump_desc_regexes:
+            matching_str_found = False
+            for regex in self.dump_desc_regexes:
+                desc_match = regex.match(dump_desc_str)
+                if desc_match:
+                    matching_str_found = True
+                    break
+            if not matching_str_found:
+                return False
+
+        self.dump_addr = dump_match.group(match_idx)
+        match_idx += 1
+        dump_data = dump_match.group(match_idx)
         # Split up dump data into hex part and ASCII part
         # A linux kernel hex_dump will always have at least two
         # spaces between the hex part and the ASCII part.
