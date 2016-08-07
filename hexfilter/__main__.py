3499f902b7fad1b2d6cc0b19a5bc43c8e90e2122
Erik Stromdahl <erik.stromdahl@gmail.com>
2016-08-07 11:05:36 +0200

    __main__.py: updated argparse strings
    Updated usage instructions
    
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

--- hexfilter/__main__.py
+++ hexfilter/__main__.py
@@ -6,10 +6,30 @@ import traceback
 import sys
 import os
 
+description = \
+"hexfilter scans input (log) files for lines containing hex dumps.\n" \
+"When a line containing a hex dump is encountered, hexfilter will write it\n" \
+"to an output file or stdout (depending on input arguments, see below).\n\n" \
+"The current version of the tool only support linux kernel hex dumps, i.e.\n" \
+"dumps produced by the print_hex_dump* functions in the kernel.\n\n" \
+"The Linux kernel can be configured to add timestamps to all log messages.\n" \
+"It is recommended to have those timestamps enabled when using hexfilter\n" \
+"since it is capable of extracting timing information from the logs.\n\n" \
+"If the kernel logs does not contain any timestamps, arguments -n or\n" \
+"--no-timestamps must be used, otherwise the hex dump data can't be\n" \
+"interpreted.\n\n"
+
+epilog = \
+"For full documentation, please visit:\n\n" \
+"http://hexfilter.readthedocs.io\n"
 
 def load_options():
     global parsed_args
-    parser = argparse.ArgumentParser(prog="hexfilter")
+    parser = argparse.ArgumentParser(prog="hexfilter",
+        description=description,
+        epilog=epilog,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+                    )
 
     parser.add_argument('-i', '--input-file',
                         help="Input (log) file to filter. If omitted, "
@@ -28,8 +48,7 @@ def load_options():
     parser.add_argument('-a', '--abs-timestamps', action="store_true",
                         help="Add absolute timestamps (keep log time "
                              "stamps as is) to the output instead of "
-                             "converting all times tamps to delta times "
-                             "(default behaviour)")
+                             "converting all times tamps to delta times")
     parser.add_argument('-r', '--rounding', type=int, default=0,
                         help="Timestamp rounding value in us."
                              "This option is not effective when absolute "
@@ -37,10 +56,10 @@ def load_options():
                              "All delta times will be rounded to the "
                              "nearest rounding step.")
     parser.add_argument('-d', '--desc-str', nargs='+', type=str,
-                        help="Description string(s) of the dumps."
+                        help="Description string(s) of the dumps. "
                              "Only dumps containing a description string "
                              "matching any of the provided desc strings "
-                             "will be filtered out."
+                             "will be filtered out. "
                              "If no --desc-str option is given, no descripton "
                              "filtering will be performed.")
     parser.add_argument('-k', '--keep-desc-str', action="store_true",
